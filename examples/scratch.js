2f172f1296c944a591dff0ff0a999cb8b027adcf
Tom Buchok <tom@buchok.com>
2014-11-17 08:34:19 -0500

    updates scratch, a pass at result data structure

--- examples/scratch.js
+++ examples/scratch.js
@@ -1,5 +1,40 @@
 vast('http://adserver.com/vast.xml')
   .on('parsed', function(data) {
-    assert(Array.isArray(data.ads));
+    data.ads.forEach(function(ad) {
+      * ad.title
+      * ad.id
+      * ad.survey
+      * ad.system.version; ad.system.value (?)
+      * ad.impressions.forEach(function(impression) {
+        * impression.id
+        * impression.url
+      });
+      ad.creatives.forEach(function(creative) {
+        * creative.sequence
+        * creative.adId
+        * creative.type => linear|companionAds
+        * creative.companionAds.forEach(function(companionAd) {
+          // ?
+        });
+        * creative.duration
+        // type => firstQuatile
+        * creative.trackingEvents['type'].forEach(function(trackingEvent) {
+          * trackingEvent.url
+        });
+        * creative.adParameters:String
+        // type => clickThrough|clickTracking
+        * creative.videoClicks['type'].forEach(function(videoClick) {
+          * videoClick.url
+          * videoClick.id
+        });
+        * creative.mediaFiles.forEach(function(mediaFile) {
+          * mediaFile.attributes...
+          * mediaFile.url
+        })
+        * creative.extensions.forEach(function(extension) {
+          extension => '<xml />'
+        })
+      });
+    });
   })
 ;
